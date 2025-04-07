85bc527e0f22dd353bf0e19a7bd4074072e4d820
MaVi <131026303+302781@users.noreply.github.com>
2025-04-07 10:12:22 -0300

    prisma

--- server.js
+++ server.js
@@ -8,9 +8,48 @@ app.user(express.json())
 
 const users = []
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
     users.push(req.body)
     await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+            password: req.body.password,
+        }
+    })
+    res.status(201).json(req.body)
+});
+app.post('/usuarios', async (req, res) => {
+    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+            password: req.body.password,
+        }
+    })
+    res.status(201).json(req.body)
+});app.post('/usuarios', async (req, res) => {
+    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+            password: req.body.password,
+        }
+    })
+    res.status(201).json(req.body)
+});
+
+app.put('/usuarios/:id', async (req, res) => {
+    console.log(req)
+    await prisma.user.update({
+        where:{
+            id: req.body.id
+        },
         data: {
             name: req.body.name,
             email: req.body.email,
@@ -24,5 +63,6 @@ app.post('/usuarios', (req, res) => {
 app.get("/usarios", (req, res) => {
     res.status(200).json(users)
 });
+
 
 app.listen(5500)
